a7f889cb99f6feb2d90d53ee87f3e5be1a107fe3
Michael Josephson <michael@simlabdevelopments.com>
2018-02-03 06:24:38 -0600

    Initialize spring boot

---
+++ src/main/java/com/mertisconsulting/cpq/CpqTranslateApplication.java
@@ -0,0 +1,12 @@
+package com.mertisconsulting.cpq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpqTranslateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpqTranslateApplication.class, args);
+	}
+}
