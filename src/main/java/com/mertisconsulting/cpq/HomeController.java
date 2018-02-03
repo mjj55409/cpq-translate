5666e918e7ade125f310b467a065069a4eaeff1f
Michael Josephson <michael@mertisconsulting.com>
2018-02-03 10:05:51 -0600

    Add template hierarchy

--- src/main/java/com/mertisconsulting/cpq/HomeController.java
+++ src/main/java/com/mertisconsulting/cpq/HomeController.java
@@ -3,7 +3,7 @@ package com.mertisconsulting.cpq;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -11,9 +11,10 @@ public class HomeController {
     @Value("${spring.application.name}")
     String appName;
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
+        model.addAttribute("message", "Welcome!");
         return "index";
     }
 }
